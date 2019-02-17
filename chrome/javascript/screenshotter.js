e3b7e0033eedf6446479b42da142f333d6965b01
alan <alan@compendiumdev.co.uk>
2019-02-17 11:30:34 +0000

    refactored the screenshot code to reduce duplication

--- chrome/javascript/screenshotter.js
+++ chrome/javascript/screenshotter.js
@@ -1,61 +1,65 @@
 'use strict';
 
-var resizing = 0;
-var scrolling = 0;
-var scrolling_timeout_milliseconds=300;
-var resize_timeout_milliseconds=300;
+/*
+  create the timeout event objects
+*/
+var resizingTimeout = {timeout: 0, milliseconds: 300};
+var scrollingTimeout = {timeout: 0, milliseconds: 300};
+var dblClickTimeout = {timeout: 0, milliseconds: 0};
+
+/*
+  SETUP THE DEFAULTS FROM THE OBSERVATRON
+*/
 
 chrome.storage.local.get(['observatron_screenshotter'], setObservatronDefaults);
 
 
 function setObservatronDefaults(options){
   if(options){
-    scrolling_timeout_milliseconds = options.observatron_screenshotter.resize_timeout;
-    resize_timeout_milliseconds = options.observatron_screenshotter.scrolling_timeout;
+    scrollingTimeout["milliseconds"] = options.observatron_screenshotter.resize_timeout;
+    resizingTimeout["milliseconds"] = options.observatron_screenshotter.scrolling_timeout;
     console.log("set defaults from observatron");
     console.log(options);
   }
 }
 
+/*
+  Configure the event listeners to pass messages back to the background.js
+*/
+window.addEventListener('resize', sendResizeMessage, false);
+window.addEventListener('scroll', sendScrollMessage, false);
+window.addEventListener('dblclick', sendDblClickMessage, false);
 
+function sendResizeMessage(){
+  sendAMessageWhenTimeoutComplete(resizingTimeout, "resize");
+}
 
-window.addEventListener('resize', function() {
-  if (resizing) {
-    clearTimeout(resizing);
+function sendDblClickMessage(){
+  sendAMessageWhenTimeoutComplete(dblClickTimeout, "screenshotdbl");
+}
+
+function sendScrollMessage(){
+  sendAMessageWhenTimeoutComplete(scrollingTimeout, "scrolled");
+}
+
+/*
+  Generic implementation for the message sending using timeouts
+*/
+function sendAMessageWhenTimeoutComplete( timeoutVariable, messageName){
+  if (timeoutVariable.timeout) {
+    clearTimeout(timeoutVariable.timeout);
   }
+
+  timeoutVariable.timeout = setTimeout(function() {
+    sendAMessage(timeoutVariable, messageName);
+  }, timeoutVariable.milliseconds);
+}
+
+function sendAMessage( timeoutVariable, messageName){
   if(chrome.runtime!=undefined){
-    resizing = setTimeout(function() {
-      chrome.runtime.sendMessage({method: 'resize'});
-      console.log("resize sent");
-      
-      resizing = 0;
-    }, resize_timeout_milliseconds);
+    chrome.runtime.sendMessage({method: messageName});
+    console.log(messageName + " sent");
+    timeoutVariable.timeout = 0;
   }
-}, false);
+}
 
-
-window.addEventListener('scroll', function(event) {
-
-  if (scrolling) {
-    clearTimeout(scrolling);
-  }
-
-  if(chrome.runtime!=undefined){
-    console.log("current timeout " +  scrolling_timeout_milliseconds);
-    scrolling = setTimeout(function() {
-      chrome.runtime.sendMessage({method: 'scrolled'});
-      console.log("scrolled sent");
-      scrolling = 0;
-    }, scrolling_timeout_milliseconds);
-  }
-
-}, false);
-
-window.addEventListener('dblclick', function(event) {
-
-  if(chrome.runtime!=undefined){
-      chrome.runtime.sendMessage({method: 'screenshotdbl'});
-      console.log("screenshot sent from double click");
-  }
-
-}, false);
